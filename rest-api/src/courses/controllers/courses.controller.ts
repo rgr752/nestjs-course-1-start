fc28aa0c2d5314862e966d07e8cbc6ba5156876f
Daniel <danielfernandesprojects@gmail.com>
2019-11-28 23:07:59 +0000

    Initial commit

---
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -0,0 +1,12 @@
+import {Controller, Get} from '@nestjs/common';
+import {Course} from '../../../../shared/course';
+import {findAllCourses} from '../../../db-data';
+
+@Controller()
+export class CoursesController {
+
+  @Get('/api/courses')
+  async findAllCourses(): Promise<Course[]> {
+      return findAllCourses() as Course[];
+  }
+}
